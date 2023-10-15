84e766e6e013cc940036b3052fc1e316e7ae1843
YeongCheeSeng <yeongcheeseng95@gmail.com>
2023-10-16 01:10:22 +0800

    added background music for each scene

--- Assets/script/FlappyBird.cs
+++ Assets/script/FlappyBird.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,29 +10,32 @@ public class FlappyBird : MonoBehaviour
     public float JumpForce = 200f;
     public float MoveForce = 400f;
 
+    public float StartGameDelay = 1f;
+    private float _timer = 0f;
+
     private Rigidbody2D _rigidBody2D;
 
 
 
 
-    private void Start()
+     void Start()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
-
+        _timer = StartGameDelay;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             // Vevtor2.up = (0,1)
             _rigidBody2D.velocity = Vector2.zero;
             _rigidBody2D.AddForce(Vector2.up * JumpForce);
         }
 
-        if (Input.GetKeyDown("left")) 
+        if (Input.GetKeyDown("left"))
         {
             _rigidBody2D.velocity = Vector2.zero;
             _rigidBody2D.AddForce(Vector2.up * JumpForce);
@@ -50,6 +53,13 @@ public class FlappyBird : MonoBehaviour
         {
             _rigidBody2D.velocity = Vector2.zero;
             _rigidBody2D.AddForce(Vector2.down * JumpForce);
+        }
+
+        if (_timer > 0f)
+        {
+            _rigidBody2D.velocity = Vector2.zero;
+            _timer -= Time.deltaTime;
+            return;
         }
 
     }
